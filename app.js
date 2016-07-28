9feb28ed38802400e3683499a86f6711772b3b24
LeoInLomrt <musicwithme0725@gmail.com>
2016-07-28 23:46:02 +0800

    不會寫自動刷新只好讓他一直refresh

--- app.js
+++ app.js
@@ -2,29 +2,37 @@ var express = require('express');
 var app = express();
 var request = require('request');
 app.set('view engine', 'ejs');
+app.set('views',__dirname + '/views');
 app.use(express.static('public'));
-
-//socket io
+// //socket io
 // var http = require('http').Server(app);
 // var io = require('socket.io')(http);
 //
 //
-//
+// var aaa=10;
 // io.on('connection', function(socket){
 //   console.log('a user connected');
 //   request('http://finance.google.com/finance/info?client=ig&q=NASDAQ:NVDA,NYSE:LN,NYSE:TWLO', function (error, response, body) {
 //     if (!error && response.statusCode == 200) {
 //       // console.log(JSON.parse(body.substring(3))); // Show the HTML for the Google homepage.
 //       var data =JSON.parse(body.substring(3));
-//       var count =[{spend:47373.05,count:1000},{spend:18483.21,count:430},{spend:4260,count:100}];
+//       var count =[{spend:47373.05,count:aaa},{spend:18483.21,count:430},{spend:4260,count:100}];
 //       // res.render('index',{jsondata:data,base:count});
-//       socket.emit('ngTableSocket', data);
+//       socket.emit('ngTableSocket', {jsondata:data,base:count});
 //     }
 //   })
-//       var users= [
-//               {"name": "Moroni", "age": Math.round(Math.random() * 100)},
-//               {"name": "Enos", "age":Math.round(Math.random() * 100)}
-//       ];
+//   // socket.on('message', function(data2) {
+//   //   request('http://finance.google.com/finance/info?client=ig&q=NASDAQ:NVDA,NYSE:LN,NYSE:TWLO', function (error, response, body) {
+//   //     if (!error && response.statusCode == 200) {
+//   //       // console.log(JSON.parse(body.substring(3))); // Show the HTML for the Google homepage.
+//   //       aaa++;
+//   //       var data =JSON.parse(body.substring(3));
+//   //       var count =[{spend:47373.05,count:aaa},{spend:18483.21,count:430},{spend:4260,count:100}];
+//   //       // res.render('index',{jsondata:data,base:count});
+//   //       socket.emit('ngTableSocket', {jsondata:data,base:count});
+//   //     }
+//   //   })
+//   // });
 // });
 //
 // http.listen(3000, function(){
@@ -32,44 +40,27 @@ app.use(express.static('public'));
 // });
 
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.broadcast.emit('jsondata', {"aaa":"aaa"});
-//   socket.on('new message', function (data2) {
-//       console.log("app.js"+data2);
-//       require('http').request({
-//           host: 'finance.google.com',
-//           path: '/finance/info?client=ig&q=NASDAQ:NVDA,NYSE:LN,NYSE:TWLO',
-//           method: 'GET'
-//       }, function(res) {
-//           res.setEncoding('utf8');
-//           var body = '';
-//           res.on('data', function(chunk) {
-//               body += chunk;
-//           });
-//           res.on('end', function() {
-//               // body = JSON.parse(body);
-//               console.log(JSON.parse(body.substring(3)));
-//               var data =JSON.parse(body.substring(3));
-//               var count =[{spend:47373.05,count:1000},{spend:18483.21,count:430},{spend:4260,count:100}];
-//               // Do stuff here
-//               socket.broadcast.emit('jsondata', data);
-//               socket.broadcast.emit('base', count);
-//           })
-//       }).end();
+// app.get('/update', function(req, res){
+//   request('http://finance.google.com/finance/info?client=ig&q=NASDAQ:NVDA,NASDAQ:GPRO', function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       console.log(JSON.parse(body.substring(3))); // Show the HTML for the Google homepage.
+//       var data =JSON.parse(body.substring(3));
+//       var count =[{spend:47373.05,count:1},{spend:11431.48,count:1000}];
 //
-//   });
-// });
-
+//       res.render('index',{jsondata:data,base:count});
+//     }
+//   })
+//
+//  });
 
 
 app.get('/', function(req, res){
-  request('http://finance.google.com/finance/info?client=ig&q=NASDAQ:NVDA,NYSE:LN,NYSE:TWLO', function (error, response, body) {
+  request('http://finance.google.com/finance/info?client=ig&q=NASDAQ:NVDA,NASDAQ:GPRO', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // console.log(JSON.parse(body.substring(3))); // Show the HTML for the Google homepage.
+      console.log(JSON.parse(body.substring(3))); // Show the HTML for the Google homepage.
       var data =JSON.parse(body.substring(3));
-      var count =[{spend:47373.05,count:1000},{spend:18483.21,count:430},{spend:4260,count:100}];
-      res.render('index',{jsondata:data,base:count});
+      var count =[{spend:47373.05,count:1000},{spend:24088.68,count:2000}];
+      res.render('index0',{jsondata:data,base:count});
 
     }
   })
